16abc99c8aa593ba0d9b67404d17d405c624ed73
Prajay Verenkar <prajay@ajency.in>
2015-11-20 11:27:08 +0530

    APi auth middle ware

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+
 /********API********/
 Route::group( ['prefix' => 'api/v1', 'middleware' => ['api_auth']], function() {
     Route::post( 'user/dosetup', 'Rest\UserController@doSetup' );
