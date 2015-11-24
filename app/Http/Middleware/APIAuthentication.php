36aa4174a127b0304bd4d28b7b09a4f7309cf943
Prajay Verenkar <prajay@ajency.in>
2015-11-24 18:58:53 +0530

    no message

--- app/Http/Middleware/APIAuthentication.php
+++ app/Http/Middleware/APIAuthentication.php
@@ -29,7 +29,7 @@ class APIAuthentication
             
             return $next($request)->header('Access-Control-Allow-Origin' , '*')
                                   ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-                                  ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With');
+                                  ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Authorization, X-Requested-With');
         
     }
 }
