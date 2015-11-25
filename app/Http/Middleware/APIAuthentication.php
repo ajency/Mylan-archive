ee5f7ae9244f18002e1ce9d985b0e5372b572125
Prajay Verenkar <prajay@ajency.in>
2015-11-25 13:17:25 +0530

    no message

--- app/Http/Middleware/APIAuthentication.php
+++ app/Http/Middleware/APIAuthentication.php
@@ -15,9 +15,15 @@ class APIAuthentication
      */
     public function handle($request, Closure $next)
     {
+        header('Access-Control-Allow-Origin: *');
+        header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
+        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, X-Authorization, X-API-KEY');
+        header('Access-Control-Allow-Credentials: true');
+
         // ALLOW OPTIONS METHOD
         $headers = [
-            'Access-Control-Allow-Headers'=> 'Content-Type,  X-Authorization, X-API-KEY, Origin'
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Origin, Content-Type, Accept, X-Authorization, X-API-KEY'
         ];
 
         $userId = (isset($reques['user_id']))?$reques['user_id']:0;
