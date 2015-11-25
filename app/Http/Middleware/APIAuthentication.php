3e7f3150760a517e8b6e7a68b1ebb7cd8272cf29
Prajay Verenkar <prajay@ajency.in>
2015-11-25 12:43:59 +0530

    no message

--- app/Http/Middleware/APIAuthentication.php
+++ app/Http/Middleware/APIAuthentication.php
@@ -15,10 +15,6 @@ class APIAuthentication
      */
     public function handle($request, Closure $next)
     {
-        header('Access-Control-Allow-Origin: *');
-        header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-        header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, X-Authorization, X-API-KEY');
-        header('Access-Control-Allow-Credentials: true');
 
         header("Access-Control-Allow-Origin: *");
 
