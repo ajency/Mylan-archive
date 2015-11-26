a1a5f5270838070cfc2c01f36dec24f66b940d18
Prajay Verenkar <prajay@ajency.in>
2015-11-26 16:55:33 +0530

    exeption handling and set password

--- app/Http/Controllers/Rest/UserController.php
+++ app/Http/Controllers/Rest/UserController.php
@@ -94,74 +94,94 @@ class UserController extends Controller
 
     public function doSetup(Request $request)
     {
-        $data = $request->all();  
-        $referenceCode = $data['referenceCode'];
-        $deviceIdentifier = $data['deviceIdentifier'];
-        $user = User::where('reference_Code',$referenceCode)->first(); 
-        
-        if($user==null)
+        try
         {
-            $json_resp = array(
-                'code' => 'invalid_reference_code' , 
-                'message' => 'reference code does not match',
-                'hospitalData' => array()
-                );
-            $status_code = 200;
-        }
-        else
-        {
-            $userId = $user['id'];
-            $hospitalId = $user['hospital_id'];
-            $projectId = $user['project_id'];
-            $hospitalData = $this -> getHospitalData($hospitalId,$projectId);
-
-            $userDeviceCount = UserDevice::where('user_id',$userId)->get()->count();
-            if($userDeviceCount >=5)
+            $data = $request->all();  
+            $referenceCode = $data['referenceCode'];
+            $deviceIdentifier = $data['deviceIdentifier'];
+            $user = User::where('reference_Code',$referenceCode)->first(); 
+            
+            if($user==null)
             {
                 $json_resp = array(
-                    'code' => 'limit_exceeded' , 
-                    'message' => 'cannot do setup more then 5 times',
-                    'hospitalData' => $hospitalData
+                    'code' => 'invalid_reference_code' , 
+                    'message' => 'reference code does not match',
+                    'hospitalData' => array()
                     );
-                    $status_code = 200;
-            }
-            elseif($userDeviceCount)
-            {
-                $userDevice = UserDevice::where(['user_id'=>$userId,'device_identifier'=>$deviceIdentifier])->get()->count(); 
-                if($userDevice)
-                {
-                    $json_resp = array(
-                    'code' => 'do_login' , 
-                    'message' => 'Device exist',
-                    'hospitalData' => $hospitalData
-                    );
-                    $status_code = 200;
-                }
-                else
-                {
-                    //add device
-                    $this->addDevice($data,$userId,$hospitalData,'do_login');
-                     $json_resp = array(
-                    'code' => 'do_login' , 
-                    'message' => 'Device exist',
-                    'hospitalData' => $hospitalData
-                    );
-                    $status_code = 200;
-                }
+                $status_code = 200;
             }
             else
             {
-                //New setup
-                $this->addDevice($data,$userId,$hospitalData,'set_password');
-                 $json_resp = array(
-                    'code' => 'set_password' , 
-                    'message' => 'New setup done',
-                    'hospitalData' => $hospitalData
-                    );
-                    $status_code = 200;
-            }
+                $userId = $user['id'];
+                $hospitalId = $user['hospital_id'];
+                $projectId = $user['project_id'];
+                $hospitalData = $this -> getHospitalData($hospitalId,$projectId);
 
+                $userDeviceCount = UserDevice::where('user_id',$userId)->get()->count();
+                if($userDeviceCount >=5)
+                {
+                    $json_resp = array(
+                        'code' => 'limit_exceeded' , 
+                        'message' => 'cannot do setup more then 5 times',
+                        'hospitalData' => $hospitalData
+                        );
+                        $status_code = 200;
+                }
+                elseif($userDeviceCount)
+                {
+                    $userDevice = UserDevice::where(['user_id'=>$userId,'device_identifier'=>$deviceIdentifier])->get()->count(); 
+                    if($userDevice && $user['password'] =='')
+                    {
+                        $json_resp = array(
+                        'code' => 'set_password' , 
+                        'message' => 'Set Password',
+                        'hospitalData' => $hospitalData
+                        );
+                        $status_code = 200;
+                    }
+                    elseif($userDevice)
+                    {
+                        $json_resp = array(
+                        'code' => 'do_login' , 
+                        'message' => 'Device exist',
+                        'hospitalData' => $hospitalData
+                        );
+                        $status_code = 200;
+                    }
+                    else
+                    {
+                        //add device
+                        $this->addDevice($data,$userId,$hospitalData,'do_login');
+                         $json_resp = array(
+                        'code' => 'do_login' , 
+                        'message' => 'Device exist',
+                        'hospitalData' => $hospitalData
+                        );
+                        $status_code = 200;
+                    }
+                }
+                else
+                {
+                    //New setup
+                    $this->addDevice($data,$userId,$hospitalData,'set_password');
+                     $json_resp = array(
+                        'code' => 'set_password' , 
+                        'message' => 'New setup done',
+                        'hospitalData' => $hospitalData
+                        );
+                        $status_code = 200;
+                }
+
+            }
+        } catch (Exception $ex) {
+
+            $json_resp = array(
+                'code' => 'Failed' , 
+                'message' => 'Some error message'
+                );
+            $status_code = 404;        
         }
+        
 
          return response()->json( $json_resp, $status_code);        
 
@@ -211,47 +231,57 @@ class UserController extends Controller
 
     public function doLogin(Request $request)
     {
-        $data = $request->all();  
-        $referenceCode = $data['referenceCode'];
-        $password = trim($data['password']);
-        $newpassword = getPassword($referenceCode , $password);
- 
-        $user = User::where('reference_Code',$referenceCode)->first(); 
-        
-        if($user==null)
-        {
-            $json_resp = array(
-                'code' => 'invalid_user' , 
-                'message' => 'In valid user'
-                );
-            $status_code = 200;
-        }
-        else
-        {
+        try{
+            $data = $request->all();  
+            $referenceCode = $data['referenceCode'];
+            $password = trim($data['password']);
+            $newpassword = getPassword($referenceCode , $password);
+     
+            $user = User::where('reference_Code',$referenceCode)->first(); 
             
-            if (Hash::check($newpassword, $user['password']))  
+            if($user==null)
             {
-                $projectId = $user['project_id'];
-                $hospitalId = $user['hospital_id'];
-                $hospitalData = $this -> getHospitalData($hospitalId,$projectId);
-                $apiKey = $user->apiKey()->first();
                 $json_resp = array(
-                    'project_id'=> $projectId,    
-                    'user-auth-key'=> $apiKey['key'],
-                    'hospital'=> $hospitalData,
-                    'code' => 'successful_login' , 
-                    'message' => 'Successfully logged in'
-                );
+                    'code' => 'invalid_user' , 
+                    'message' => 'In valid user'
+                    );
                 $status_code = 200;
             }
             else
             {
-               $json_resp = array(
-                'code' => 'invalid_login' , 
-                'message' => 'Invalid Login details'
-                );
-                $status_code = 200; 
+                
+                if (Hash::check($newpassword, $user['password']))  
+                {
+                    $projectId = $user['project_id'];
+                    $hospitalId = $user['hospital_id'];
+                    $hospitalData = $this -> getHospitalData($hospitalId,$projectId);
+                    $apiKey = $user->apiKey()->first();
+                    $json_resp = array(
+                        'project_id'=> $projectId,    
+                        'user-auth-key'=> $apiKey['key'],
+                        'hospital'=> $hospitalData,
+                        'code' => 'successful_login' , 
+                        'message' => 'Successfully logged in'
+                    );
+                    $status_code = 200;
+                }
+                else
+                {
+                   $json_resp = array(
+                    'code' => 'invalid_login' , 
+                    'message' => 'Invalid Login details'
+                    );
+                    $status_code = 200; 
+                }
             }
+        }
+        catch (Exception $ex) {
+
+            $json_resp = array(
+                'code' => 'Failed' , 
+                'message' => 'Some error message'
+                );
+            $status_code = 404;        
         }
 
         return response()->json( $json_resp, $status_code); 
@@ -259,32 +289,42 @@ class UserController extends Controller
 
     public function setPassword(Request $request)
     {
-        $data = $request->all();  
-        $referenceCode = $data['referenceCode'];
-        $password = trim($data['password']);
-        $newpassword = getPassword($referenceCode , $password);
- 
-        $user = User::where('reference_Code',$referenceCode)->first(); 
-        
-        if($user==null)
-        {
-            $json_resp = array(
-                'code' => 'invalid_user' , 
-                'message' => 'In valid user'
-                );
-            $status_code = 200;
-        }
-        else
-        {    
-            $user->password = Hash::make($newpassword);
-            $user->save();
-
+        try{
+            $data = $request->all();  
+            $referenceCode = $data['referenceCode'];
+            $password = trim($data['password']);
+            $newpassword = getPassword($referenceCode , $password);
+     
+            $user = User::where('reference_Code',$referenceCode)->first(); 
+            
+            if($user==null)
+            {
                 $json_resp = array(
-                'code' => 'do_login' , 
-                'message' => 'Setup Successfully done'
-                );
+                    'code' => 'invalid_user' , 
+                    'message' => 'In valid user'
+                    );
                 $status_code = 200;
-       
+            }
+            else
+            {    
+                $user->password = Hash::make($newpassword);
+                $user->save();
+
+                    $json_resp = array(
+                    'code' => 'do_login' , 
+                    'message' => 'Setup Successfully done'
+                    );
+                    $status_code = 200;
+           
+            }
+        }
+        catch (Exception $ex) {
+
+            $json_resp = array(
+                'code' => 'Failed' , 
+                'message' => 'Some error message'
+                );
+            $status_code = 404;        
         }
 
         return response()->json( $json_resp, $status_code); 
@@ -292,28 +332,38 @@ class UserController extends Controller
 
     public function userApiKey(Request $request)
     {
-        $data = $request->all();  
-        $referenceCode = $data['referenceCode'];
-        $user = User::where('reference_Code',$referenceCode)->first(); 
-        
-        if($user==null)
-        {
-            $json_resp = array(
-                'code' => 'invalid_user' , 
-                'message' => 'In valid user'
+        try{
+            $data = $request->all();  
+            $referenceCode = $data['referenceCode'];
+            $user = User::where('reference_Code',$referenceCode)->first(); 
+            
+            if($user==null)
+            {
+                $json_resp = array(
+                    'code' => 'invalid_user' , 
+                    'message' => 'In valid user'
+                    );
+                $status_code = 200;
+            }
+            else
+            {
+                $apiKey = $user->apiKey()->first();
+                $json_resp = array(
+                    'user-auth-key'=> $apiKey['key'],
+                    'code' => 'successful_login' , 
+                    'message' => 'Successfully logged in'
                 );
-            $status_code = 200;
+                $status_code = 200;
+                 
+            }
         }
-        else
-        {
-            $apiKey = $user->apiKey()->first();
+        catch (Exception $ex) {
+
             $json_resp = array(
-                'user-auth-key'=> $apiKey['key'],
-                'code' => 'successful_login' , 
-                'message' => 'Successfully logged in'
-            );
-            $status_code = 200;
-             
+                'code' => 'Failed' , 
+                'message' => 'Some error message'
+                );
+            $status_code = 404;        
         }
 
         return response()->json( $json_resp, $status_code); 
