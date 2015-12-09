b332f753710bb4a7a50e9d8099d206736c81d342
Prajay Verenkar <prajay@ajency.in>
2015-12-09 19:19:59 +0530

    login api changes

--- app/Http/Controllers/Rest/UserController.php
+++ app/Http/Controllers/Rest/UserController.php
@@ -249,6 +249,7 @@ class UserController extends Controller
         try{
             $data = $request->all();  
             $referenceCode = $data['referenceCode'];
+            $installationId = $data['installationId'];
             $password = trim($data['password']);
             $newpassword = getPassword($referenceCode , $password);
      
@@ -267,20 +268,35 @@ class UserController extends Controller
                 
                 if (Hash::check($newpassword, $user['password']) && $user['account_status']=='active')  
                 {
+
                     $projectId = $user['project_id'];
                     $hospitalId = $user['hospital_id'];
-                    $data = $this -> postLoginData($hospitalId,$projectId);
-                    $hospitalData = $data['hospital']; 
-                    $questionnaireData = $data['questionnaire']; 
                     $apiKey = $user->apiKey()->first();
-                    $json_resp = array( 
-                        'user-auth-key'=> $apiKey['key'],
-                        'hospital'=> $hospitalData,
-                        'questionnaire'=> $questionnaireData,
-                        'code' => 'successful_login' , 
-                        'message' => 'Successfully logged in'
-                    );
-                    $status_code = 200;
+                    $parseUser = $this->getParseUser($referenceCode,$installationId,$apiKey);
+                    if($parseUser!='error')
+                    {
+                        $data = $this -> postLoginData($hospitalId,$projectId);
+                        $hospitalData = $data['hospital']; 
+                        $questionnaireData = $data['questionnaire']; 
+                        $parseUser =json_decode($parseUser,true); 
+                        $json_resp = array( 
+                            'user'=> $parseUser['result'],
+                            'hospital'=> $hospitalData,
+                            'questionnaire'=> $questionnaireData,
+                            'code' => 'successful_login' , 
+                            'message' => 'Successfully logged in'
+                        );
+                        $status_code = 200;
+                    }
+                    else
+                    {
+                        $json_resp = array(
+                        'code' => 'invalid_login' , 
+                        'message' => 'Invalid Login details'
+                        );
+                        $status_code = 200;
+                    }
+                    
                 }
                 else
                 {
@@ -302,6 +318,45 @@ class UserController extends Controller
         }
 
         return response()->json( $json_resp, $status_code); 
+    }
+
+    public function getParseUser($referenceCode,$installationId,$authKey)
+    {
+
+
+        $headers = array(
+            "X-Parse-Application-Id: MQiH2NRh0G6dG51fLaVbM0i7TnxqX2R1pKs5DLPA",
+            "X-Parse-REST-API-Key: I4yEHhjBd4e9x28MvmmEOiP7CzHCVXpJxHSu5Xva",
+            "X-Parse-Master-Key: xI28t663DC4wTelQQkfOnY1OOwj39sAHtluZk10i"
+        );
+
+        $objectData = '{"authKey":"'.$authKey.'", "referenceCode":"'.$referenceCode.'", "installationId":"'.$installationId.'"}';  
+
+        $c = curl_init(); 
+        curl_setopt($c, CURLOPT_URL, 'https://api.parse.com/1/functions/loginParseUser');
+        curl_setopt($c,CURLOPT_POST,1);  
+        curl_setopt($c,CURLOPT_POSTFIELDS,$objectData); 
+        curl_setopt($c, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($c, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($c, CURLOPT_SSL_VERIFYPEER, 0);
+        $o = curl_exec($c); 
+
+        if (curl_errno($c)) {
+            $sad = curl_error($c);
+            print_r($sad);
+            throw new Exception($sad);
+        }   
+
+        $info=curl_getinfo($c,CURLINFO_HTTP_CODE);
+        curl_close($c);
+        
+        if($info==200){
+            return $o;
+        }else{
+            return "error";
+        }
     }
 
     public function setPassword(Request $request)
