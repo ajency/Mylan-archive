ba5640043702735e126a41b7f674019b215aef94
namrata salaskar <namrata@ajency.in>
2016-01-08 16:43:51 +0530

    summary number with date

--- public/patients/js/questionnaire/questionnaire-api.js
+++ public/patients/js/questionnaire/questionnaire-api.js
@@ -2,22 +2,11 @@ angular.module('angularApp.questionnaire').factory('QuestionAPI', [
   '$q', '$http', 'App', function($q, $http, App) {
     var QuestionAPI;
     QuestionAPI = {};
-    QuestionAPI.getSummary = function(id) {
-      var PARSE_HEADERS, PARSE_URL, defer, param, url;
+    QuestionAPI.getSummary = function(options) {
+      var defer;
       defer = $q.defer();
-      PARSE_URL = 'https://api.parse.com/1/functions';
-      url = PARSE_URL + '/getSummary';
-      param = {
-        'responseId': id
-      };
-      PARSE_HEADERS = {
-        headers: {
-          "X-Parse-Application-Id": 'MQiH2NRh0G6dG51fLaVbM0i7TnxqX2R1pKs5DLPA',
-          "X-Parse-REST-API-KeY": 'I4yEHhjBd4e9x28MvmmEOiP7CzHCVXpJxHSu5Xva'
-        }
-      };
-      $http.post(url, param, PARSE_HEADERS).then(function(data) {
-        return defer.resolve(data.data);
+      App.SendParseRequest('getSummary', options).then(function(data) {
+        return defer.resolve(data);
       }, (function(_this) {
         return function(error) {
           return defer.reject(error);
--- public/patients/source/questionnaire/questionnaire-api.coffee
+++ public/patients/source/questionnaire/questionnaire-api.coffee
@@ -3,26 +3,12 @@ angular.module 'angularApp.questionnaire'
 .factory 'QuestionAPI', ['$q', '$http', 'App', ($q, $http, App)->
 	QuestionAPI = {}
 
-	QuestionAPI.getSummary = (id)->
-		defer = $q.defer()
-
-		PARSE_URL = 'https://api.parse.com/1/functions'
-
-		url = PARSE_URL+'/getSummary'
-
-		# 6xS787uoTf
-
-		param =
-			'responseId' : id
-
-		PARSE_HEADERS =
-			headers:
-				"X-Parse-Application-Id" : 'MQiH2NRh0G6dG51fLaVbM0i7TnxqX2R1pKs5DLPA'
-				"X-Parse-REST-API-KeY" : 'I4yEHhjBd4e9x28MvmmEOiP7CzHCVXpJxHSu5Xva'
-				
-		$http.post url,  param, PARSE_HEADERS
+	QuestionAPI.getSummary = (options)->
+		
+		defer = $q.defer()			
+		App.SendParseRequest('getSummary', options)
 		.then (data)->
-			defer.resolve data.data
+			defer.resolve data
 		, (error)=>
 			defer.reject error
 			
