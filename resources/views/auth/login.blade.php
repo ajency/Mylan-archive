9dec600d384b2dfc7eea476892c41ab15776a2e2
Ajency <alisha@ajency.in>
2016-01-11 12:34:10 +0530

    loader added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,7 +9,7 @@
            <div class="text-right btn-contact" style="margin-top:-85px;"><button type="submit" class="btn btn-default">Contact Us</button></div>
            <br class="hidden-xs">
            <br>
-        <div class="bg-white shadow-full">
+        <div class="bg-white shadow-full" style="border:1px solid #ddd;">
           <div class="row" style="position:relative;">
             <div class="img-bg hidden-xs">
             </div>
