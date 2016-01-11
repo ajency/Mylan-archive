07863da3386b1728da12fe194c7460d5fd52b660
Prajay Verenkar <prajay@ajency.in>
2016-01-11 19:28:30 +0530

    patient submission data

--- app/Http/Controllers/Hospital/HospitalController.php
+++ app/Http/Controllers/Hospital/HospitalController.php
@@ -479,17 +479,23 @@ class HospitalController extends Controller
         $schedules = $scheduleQry->find();
 
         $patientNextOccurrence = [];
+        $patientResponses = [];
         foreach($schedules as $schedule)
         {
             $patientId = $schedule->get("patient");
             $nextOccurrence = $schedule->get("nextOccurrence")->format('dS M');
-            $patientNextOccurrence[$patientId]=$nextOccurrence;
+            // $patientNextOccurrence[$patientId]=$nextOccurrence;
+            $patientResponses[$patientId]['nextSubmission'] = $nextOccurrence;
+            $patientResponses[$patientId]['totalFlags']=[];
+            $patientResponses[$patientId]['missed'] =[];
+            $patientResponses[$patientId]['count'] =[];
+            $patientResponses[$patientId]['lastSubmission'] = '-' ;
 
         }
 
         $responses = $this->getPatientsResponses($patients,$projectId,0,[] ,$startDate,$endDate); 
         $completedResponses = [];
-        $patientResponses = [];
+        
         
         foreach ($responses as $key => $response) {
             $status = $response->get("status");
@@ -500,9 +506,6 @@ class HospitalController extends Controller
             if(!isset($patientResponses[$patient]))
             {
                 $patientResponses[$patient]['lastSubmission'] = $occurrenceDate;
-                $patientResponses[$patient]['nextSubmission'] = $patientNextOccurrence[$patient];
-                $patientResponses[$patient]['totalFlags']=[];
-                $patientResponses[$patient]['missed'] =[];
             }
 
             $patientResponses[$patient]['count'][]=$responseId;
