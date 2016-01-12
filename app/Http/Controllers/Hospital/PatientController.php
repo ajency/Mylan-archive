d2c5f8d14a48fdb68ac46df432cde21433288361
Ajency <alisha@ajency.in>
2016-01-12 11:49:52 +0530

    no message

--- app/Http/Controllers/Hospital/PatientController.php
+++ app/Http/Controllers/Hospital/PatientController.php
@@ -97,7 +97,8 @@ class PatientController extends Controller
         $apiKey->save();
 
  
-        return redirect(url($hospitalSlug . '/patients/' . $userId)); 
+        //return redirect(url($hospitalSlug . '/patients/' . $userId)); 
+        return redirect(url($hospitalSlug . '/patients/' . $userId . '/base-line-score-edit')); 
     }
 
     /**
@@ -322,6 +323,11 @@ class PatientController extends Controller
             {
                 foreach ($answers as  $options) {
                     $answerData = explode('-',$options);
+                    
+                    if(count($answerData)<=1)
+                        continue;
+
+                    
                     $optionId = $answerData[0];
                     $score = intval ($answerData[1]);
 
