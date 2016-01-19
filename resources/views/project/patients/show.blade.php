0d563de3bd71d094367565740086aa249219001a
Prajay Verenkar <prajay@ajency.in>
2016-01-19 19:29:08 +0530

    latest 5 submission in patient summry

--- resources/views/project/patients/show.blade.php
+++ resources/views/project/patients/show.blade.php
@@ -185,55 +185,67 @@
                                        
                                        <!-- <h6>This patient has missed 5 consecutive submissions</h6> -->
                  
-                                       <table class="table table-hover table-flip-scroll cf">
-                                          <thead class="cf">
-                                             <tr>
-                                                <th>Submission Date</th>
-                                                <th>Sequence Number</th>
-                                                <th>Number of Flags</th>
-                                                <th>Open Flags</th>
-                                                <th>Status</th>
-                                             </tr>
-                                          </thead>
-                                          <tbody>
-                                       <tr onclick="window.document.location='p1-submission6.html';">
-                                          <td>6/12/2015</td>
-                                          <td>12</td>
-                                          <td>0</td>
-                                          <td>0</td>
-                                          <td><span class="label label-warning">Pending Review</span></td>
-                                       </tr>                                       
-                                       <tr onclick="window.document.location='p1-submission5.html';">
-                                          <td>6/7/2015</td>
-                                          <td>7</td>
-                                          <td>0</td>
-                                          <td>0</td>
-                                          <td><span class="label label-success">Reviewed</span></td>
-                                       </tr>
+                                       <table class="table table-flip-scroll dashboard-tbl">
+                              <thead class="cf">
+                                 <tr> 
+                                    <th class="sorting" width="16%">Patient ID</th>
+                                    <th class="sorting">Submission#</th>
+                                    <th class="sorting" width="22%">Total Score</th>
+                                    <th class="sorting">Compared To Previous
+                                     <br> <sm><i class="fa fa-flag text-error"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                      <sm><i class="fa fa-flag text-warning"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                      <sm><i class="fa fa-flag text-success"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                    </th>
+                                    <th class="sorting">Compared To Baseline
+                                    <br> <sm><i class="fa fa-flag text-error"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                      <sm><i class="fa fa-flag text-warning"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                      <sm><i class="fa fa-flag text-success"></i>  <i class="iconset top-down-arrow"></i></sm>
+                                    </th>
+                                    
+                                 </tr>
+                              </thead>
+                              <tbody>
+                              <?php 
+                                $i=1;
+                              ?>
+                              @foreach($submissionsSummary as $responseId=>$responseData)
+                                <?php 
+                                  if($i==6)
+                                    break;
+                                ?>
+                                 <tr onclick="window.document.location='/{{ $hospital['url_slug'] }}/{{ $project['project_slug'] }}/submissions/{{$responseId}}';">
+                                    <td class="text-center">{{ $responseData['patient'] }}</td>
+                                    <td class="text-center">
+                                      
+                                        <h4 class="semi-bold margin-none flagcount">{{ $responseData['sequenceNumber'] }} on</h4>
+                                        <sm>{{ $responseData['occurrenceDate'] }}</sm>
+                                    
+                                    </td>
+                                     <td class="text-center">
+                                     <h3 class="bold margin-none pull-left p-l-10">{{ $responseData['totalScore'] }}</h3>
+                                     <sm class="text-muted sm-font">Prev - {{ $responseData['previousScore'] }} <i class="fa fa-flag "></i> </sm><br>
+                                      <sm class="text-muted sm-font">Base - {{ $responseData['baseLineScore'] }} <i class="fa fa-flag "></i> </sm>
+                                    </td>  
+                                     
+                                     <td class="text-center sorting">
+                                     <span class="badge badge-important">{{ count($responseData['previousFlag']['red']) }}</span>
+                                      <span class="badge badge-warning">{{ count($responseData['previousFlag']['amber']) }}</span>
+                                     <span class="badge badge-success">{{ count($responseData['previousFlag']['green']) }}</span>
+                                    </td>   
+                                         <td class="text-center sorting">
+                                     <span class="badge badge-important">{{ count($responseData['baseLineFlag']['red']) }}</span>
+                                      <span class="badge badge-warning">{{ count($responseData['baseLineFlag']['amber']) }}</span>
+                                     <span class="badge badge-success">{{ count($responseData['baseLineFlag']['green']) }}</span>
+                                    </td>  
 
-                                       <tr onclick="window.document.location='p1-submission4.html';">
-                                          <td>6/6/2015</td>
-                                          <td>6</td>
-                                          <td>6</td>
-                                          <td>5</td>
-                                          <td><span class="label label-success">Reviewed</span></td>
-                                       </tr>
-                                       <tr onclick="window.document.location='p1-submission3.html';">
-                                          <td>6/5/2015</td>
-                                          <td>5</td>
-                                          <td>4</td>
-                                          <td>3</td>
-                                          <td><span class="label label-success">Reviewed</span></td>
-                                       </tr>                                       
-                                       <tr onclick="window.document.location='p1-submission2.html';">
-                                          <td>6/2/2015</td>
-                                          <td>2</td>
-                                          <td>3</td>
-                                          <td>2</td>
-                                          <td><span class="label label-success">Reviewed</span></td>
-                                       </tr>
-                                    </tbody>
-                                       </table>
+                                </tr>
+                                <?php 
+                                $i++;
+                                ?>
+                            @endforeach
+                                  
+                              </tbody>
+                     </table>
 
                                        
                                        
