cb83e8720e214eaa0bd7167e1b465c852c01b82e
Ajency <alisha@ajency.in>
2016-02-01 12:41:52 +0530

    logo fix

--- resources/views/patient/dashbord.blade.php
+++ resources/views/patient/dashbord.blade.php
@@ -39,7 +39,7 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="#"><img src="<?php echo $hospital['logo'] ?>" width="70"></a>
+        <a class="navbar-brand" href="#"><img src="<?php echo $hospital['logo'] ?>" width="auto" haight="45px"></a>
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
