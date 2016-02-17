9e0a9dafde0925dcf897cf576734d517d7abb853
Prajay Verenkar <prajay@ajency.in>
2016-02-17 15:30:16 +0530

    date filters

--- resources/views/layouts/single-project.blade.php
+++ resources/views/layouts/single-project.blade.php
@@ -211,13 +211,12 @@
 
          $('#reportrange').daterangepicker({
             ranges: {
-            'Today': [moment(), moment()],
-            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
             'This Month': [moment().startOf('month'), moment().endOf('month')],
-            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-            }
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'Last 6 Months': [moment().month(-6), moment()],
+            'All Time': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+              
+           }
          }, cb);
  
       });
