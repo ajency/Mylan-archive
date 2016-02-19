a7d1e32794b929969d254cc98cfae24929ab0422
Prajay Verenkar <prajay@ajency.in>
2016-02-19 20:43:49 +0530

    no message

--- resources/views/project/patients/list.blade.php
+++ resources/views/project/patients/list.blade.php
@@ -133,14 +133,14 @@
                               <br> 
                               <sm class="pull-left" style="margin-left: 26px"><i class="fa fa-flag text-error"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
                               <sm style="position: relative; bottom: 2px;"><i class="fa fa-flag text-warning"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
-                              <sm class="pull-right" style="margin-right: 26px"><i class="fa fa-flag text-success"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
+                              <sm class="pull-right" style="margin-right: 35px"><i class="fa fa-flag text-success"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
                            </th>
                            <th colspan="3" class="sorting">
                               Compared To Baseline
                               <br> 
                               <sm class="pull-left" style="margin-left: 26px"><i class="fa fa-flag text-error"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
                               <sm style="position: relative; bottom: 2px;"><i class="fa fa-flag text-warning"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
-                              <sm class="pull-right" style="margin-right: 26px"><i class="fa fa-flag text-success"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
+                              <sm class="pull-right" style="margin-right: 35px"><i class="fa fa-flag text-success"></i>  <!-- <i class="iconset top-down-arrow"></i> --></sm>
                            </th>
                            <th class="sorting">Graph <br> 
                                <sm>  <i class="fa fa-circle"></i> Baseline   &nbsp; &nbsp;<i class="fa fa-circle text-warning"></i> Total Score</sm>
