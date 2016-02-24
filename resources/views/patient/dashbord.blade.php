0e14dde23ce43512fbabceca6b6846cfa5b290d4
namrata salaskar <namrata@ajency.in>
2016-02-24 10:06:10 +0530

    issue fixes #405

--- resources/views/patient/dashbord.blade.php
+++ resources/views/patient/dashbord.blade.php
@@ -41,14 +41,14 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand" href="">{{ hospitalImageExist($hospital) }}</a>
+        <a class="navbar-brand" href="#dashboard">{{ hospitalImageExist($hospital) }}</a>
        
       </div>
 
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
        <ul class="nav navbar-nav navbar-right">
-          <li><a href="#"><span class="badge-round green"><i class="fa fa-phone"></i></span>&nbsp;&nbsp;<span class="text-muted"> CALL US</span>&nbsp;&nbsp;0161 123 1234</a></li>
+          <li><a href=""><span class="badge-round green"><i class="fa fa-phone"></i></span>&nbsp;&nbsp;<span class="text-muted"> CALL US</span>&nbsp;&nbsp;0161 123 1234</a></li>
           <li ng-controller="headerCtrl" ng-include src="'patients/views/notificationcount.html'">
            <!--  <div ng-include src="'patients/views/notificationcount.html'"> -->
           <!--   <a href="#notification" ng-init="view.init()">
