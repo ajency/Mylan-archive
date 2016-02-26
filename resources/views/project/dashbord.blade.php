7d9f323668ac0c5b7f7d212c7640d1948af0c879
Prajay Verenkar <prajay@ajency.in>
2016-02-26 14:16:45 +0530

    no message

--- resources/views/project/dashbord.blade.php
+++ resources/views/project/dashbord.blade.php
@@ -156,7 +156,7 @@
                            @if(!empty($prejectAlerts['alertMsg']))
                             @foreach($prejectAlerts['alertMsg'] as $prejectAlert)
                               <div class="notification-messages {{ $prejectAlert['class'] }}" onclick="window.document.location='/{{ $hospital['url_slug'] }}/{{ $project['project_slug'] }}/submissions/{{ $prejectAlert['responseId'] }}';">
-                                <div class="message-wrapper">
+                                <div class="message-wrapper msg-card">
                                      <div class="heading">Patient ID {{ $prejectAlert['patient'] }}   </div>
                                      <div class="description"> {{ sprintf($prejectAlert['msg'], $prejectAlert['sequenceNumber'] ) }} </div>
                                   </div>
