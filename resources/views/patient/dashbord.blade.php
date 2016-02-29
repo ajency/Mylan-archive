1c3c36588fda763d542057efaf1a926dd1ae7bb6
Tanvi Bakhale <tanvi.b@ajency.in>
2016-02-29 16:45:56 +0530

    notification

--- resources/views/patient/dashbord.blade.php
+++ resources/views/patient/dashbord.blade.php
@@ -48,7 +48,7 @@
       <!-- Collect the nav links, forms, and other content for toggling -->
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
        <ul class="nav navbar-nav navbar-right">
-          <li><a href=""><span class="badge-round green"><i class="fa fa-phone"></i></span>&nbsp;&nbsp;<span class="text-muted"> CALL US</span>&nbsp;&nbsp;0161 123 1234</a></li>
+          <li><a href=""><span class="badge-round green"><i class="fa fa-phone"></i></span>&nbsp;&nbsp;<span class="text-muted"> Call Us</span>&nbsp;&nbsp;0161 123 1234</a></li>
           <li ng-controller="headerCtrl" ng-include src="'patients/views/notificationcount.html'">
            <!--  <div ng-include src="'patients/views/notificationcount.html'"> -->
           <!--   <a href="#notification" ng-init="view.init()">
--- resources/views/project/dashbord.blade.php
+++ resources/views/project/dashbord.blade.php
@@ -45,7 +45,7 @@
                   <div class="row">
             <div class="col-sm-6 ">
 <div class="tiles white">
-   <a href="{{ url($hospital['url_slug'].'/'.$project['project_slug'].'/patients') }}">
+   <a href="{{ url($hospital['url_slug'].'/'.$project['project_slug'].'/patients?patients=created') }}">
                            <div class="tiles-body" style="    padding: 6px 18px 6px 24px;">
                           <h4> <i class="fa fa-users text-success m-r-5"></i> Total Recruited Patients: <b class="bigger text-success pull-right">{{ $allpatientscount }} </b> </h4>
                          </div>
@@ -228,7 +228,7 @@
                             </div>
                           </div>
                            <div class="grid-body no-border table-data ">                              
-                              <div id="chartdiv" style="width:100%;height: 400px;"></div>
+                              <div id="chartdiv" style="width:100%;"></div>
                            </div>
                         </div>
                   <div class="grid simple grid-table">
@@ -273,9 +273,9 @@
                                    <sm style="position: relative; bottom: 2px;"  class="sortSubmission" sort="baseLineTotalAmberFlags" sort-type="asc"><i class="fa fa-flag text-warning"></i>  <i class="fa fa-angle-down sortCol"></i></sm>
                                    <sm class="pull-right sortSubmission" sort="baseLineTotalGreenFlags" sort-type="asc" style="margin-right: 20px"><i class="fa fa-flag text-success"></i>  <i class="fa fa-angle-down sortCol"></i></sm>
                                 </th>
-                                <th class="sorting sortSubmission" sort="status" sort-type="asc"  style="cursor:pointer;">Status <i class="fa fa-angle-down sortCol"></i><br><br>
+                                <th class="sorting">Status<br><br>
                                 </th>
-                                <th class="sorting sortSubmission" sort="reviewed" sort-type="asc"  style="cursor:pointer;">Review Status <i class="fa fa-angle-down sortCol"></i><br><br>
+                                <th class="sorting">Review Status<br><br>
                                 </th>
                              </tr>
                           </thead>
@@ -356,7 +356,7 @@
                         
                             @endforeach
                         @else 
-                        <tr><td class="text-center no-data-found" colspan="15"><i class="fa fa-2x fa-frown-o"></i><br>No data found</td></tr>
+                        <tr><td class="text-center no-data-found" colspan="16"><i class="fa fa-2x fa-frown-o"></i><br>No data found</td></tr>
                         @endif    
                                 
                           </tbody>
@@ -382,12 +382,7 @@
                      <thead class="cf">
                         <tr>
                            <th width="12%">Patient ID</th>
-                           <th width="35%">Total Submissions
-                            <br> 
-                              <sm class="text-success sortPatientSummary" sort="completed" sort-type="asc" style="cursor:pointer;">Completed <i class="fa fa-angle-down sortCol"></i></sm>
-                              <sm class="text-warning sortPatientSummary" sort="late" sort-type="asc" style="cursor:pointer;">Late <i class="fa fa-angle-down sortCol"></i></sm>
-                              <sm class="text-danger sortPatientSummary" sort="missed" sort-type="asc" style="cursor:pointer;">Missed <i class="fa fa-angle-down sortCol"></i></sm>
-                              </th>
+                           <th width="35%">Total Submissions</th>
                            <th colspan="3" class="sorting">
                               Compared To Previous
                               <br> 
@@ -451,11 +446,11 @@
                            <td  onclick="window.document.location='{{ url($hospital['url_slug'].'/'.$project['project_slug'].'/patients/'.$patientId) }}'">
                               <div class="lst-sub">
                                  <h2 class="bold pull-left">
-                                    {{ $patientSummary['completed'] }}<br>
+                                    {{ count($patientSummary['completed']) }}<br>
                                     <sm class="text-success">Completed</sm>
                                  </h2>
                                  <h2 class="bold pull-left">
-                                    {{ $patientSummary['late'] }}<br>
+                                    {{ count($patientSummary['late']) }}<br>
                                     <sm class="text-warning">Late</sm>
                                  </h2>
                                  <h2 class="bold pull-left">
@@ -501,7 +496,7 @@
                           ?>
                         @endforeach
                     @else 
-                        <tr><td class="text-center no-data-found" colspan="15"><i class="fa fa-2x fa-frown-o"></i><br>No data found</td></tr>
+                        <tr><td class="text-center no-data-found" colspan="16"><i class="fa fa-2x fa-frown-o"></i><br>No data found</td></tr>
                     @endif 
                      </tbody>
                   </table>
