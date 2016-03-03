2ff68394531f359eed67249d4f708c70e947aea5
Prajay Verenkar <prajay@ajency.in>
2016-03-03 15:34:17 +0530

    no message

--- resources/views/project/questionnaire-setting.blade.php
+++ resources/views/project/questionnaire-setting.blade.php
@@ -28,27 +28,30 @@
                        <h3 class="">MRI PANCREATITIS</h3>
                       
                       <hr>
-         <form class="form-horizontal" method="post" action="{{ url( $hospital['url_slug'].'/'.$project['project_slug'].'/questionnaire-setting/' ) }}">
+         <form class="form-horizontal col-sm-6 mri-form" method="post" action="{{ url( $hospital['url_slug'].'/'.$project['project_slug'].'/questionnaire-setting/' ) }}">
   <div class="form-group">
-    <label for="frequency" class="col-sm-6 control-label">Frequency</label>
+    <label for="frequency" class="col-sm-4 side-label">Frequency</label>
     <div class="col-sm-6">
       <input type="text" name="frequency" class="form-control" id="frequency" placeholder="Frequency" value="{{ $settings['frequency'] }}">
     </div>
+    <div class="col-sm-2"><h6 class="seconds">seconds</h6></div>
   </div>
  <div class="form-group">
-    <label for="gracePeriod" class="col-sm-6 control-label">Grace Period</label>
+    <label for="gracePeriod" class="col-sm-4 side-label">Grace Period</label>
     <div class="col-sm-6">
       <input type="text" class="form-control" id="gracePeriod" name="gracePeriod" placeholder="Grace Period" value="{{ $settings['gracePeriod'] }}">
     </div>
+    <div class="col-sm-2"><h6 class="seconds">seconds</h6></div>
   </div>
    <div class="form-group">
-    <label for="reminderTime" class="col-sm-6 control-label">Reminder Time</label>
+    <label for="reminderTime" class="col-sm-4 side-label">Reminder Time</label>
     <div class="col-sm-6">
       <input type="text" class="form-control" name="reminderTime" id="reminderTime" placeholder="Reminder Time" value="{{ $settings['reminderTime'] }}">
     </div>
+    <div class="col-sm-2"><h6 class="seconds">seconds</h6></div>
   </div>
  <div class="form-group">
-    <label for="editable" class="col-sm-6 control-label">Editable</label>
+    <label for="editable" class="col-sm-4 side-label">Editable</label>
     <div class="col-sm-6">
    <div class="radio">
 <input id="YES" type="radio" name="editable" value="yes" checked="checked">
@@ -59,7 +62,7 @@
     </div>
   </div>
    <div class="form-group">
-    <label for="Frequency" class="col-sm-6 control-label">Questionnaire Type</label>
+    <label for="Frequency" class="col-sm-4 side-label">Questionnaire Type</label>
     <div class="col-sm-6">
 <select id="type" name="type" style="width:100%" class="">
 <option value="random" {{ ($settings['type']=='random')?'selected':'' }}>Random</option>
@@ -68,7 +71,7 @@
     </div>
   </div>
   <div class="form-group">
-    <div class="col-sm-offset-6 col-sm-6">
+    <div class="col-sm-10 text-center mri-submit">
     <input type="hidden" value="{{ csrf_token()}}" name="_token"/>
       <button type="submit" class="btn btn-success">Save</button>
     </div>
