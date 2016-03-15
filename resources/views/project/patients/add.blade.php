50c32bcfd2279a09d371e3fc6ec4f41bbb357c8a
Prajay Verenkar <prajay@ajency.in>
2016-03-15 19:31:10 +0530

    patient add frequency

--- resources/views/project/patients/add.blade.php
+++ resources/views/project/patients/add.blade.php
@@ -132,8 +132,24 @@
                         
                      
                   </div>
+                  <hr>
+               <h4 class="no-margin">Set <span class="semi-bold">Frequency</span></h4>
+               <br>
+                <div class="row form-group">
                   
-                  <button type="button" class="btn btn-link text-success add-visit"><i class="fa fa-plus"></i> Add Visit</button>
+                  <div class="col-sm-4">
+                    <input type="text" name="frequencyDay" class="form-control" id="frequency" placeholder="Frequency" value="" data-parsley-type="number"> <h6 class="seconds">days</h6>
+                  </div>
+                  
+                  <div class="col-sm-4">
+                    <input type="text" name="frequencyHours" class="form-control" id="frequency" placeholder="Frequency" value="" data-parsley-type="number"><h6 class="seconds">hours</h6>
+                  </div>
+                 
+                </div>
+               
+     
+                  
+                
         
                
         <div class="form-actions">
