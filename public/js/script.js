41a06e80219dfcc5478fc0db0d9e98f471db2e4a
Prajay Verenkar <prajay@ajency.in>
2016-03-16 13:56:53 +0530

    no message

--- public/js/script.js
+++ public/js/script.js
@@ -431,6 +431,8 @@ function lineChartWithOutBaseLine(chartData,legends,container,xaxisLable,yaxisLa
         return graphObj;
     })
    
+    // chartDataLength = Object.keys(chartData).length;
+
 
     var chart = AmCharts.makeChart(container, {
         "type": "serial",
@@ -471,7 +473,7 @@ function lineChartWithOutBaseLine(chartData,legends,container,xaxisLable,yaxisLa
         "categoryAxis": {
             "gridPosition": "start",
             "markPeriodChange": false,
-            "minHorizontalGap": 100,
+            "minHorizontalGap": 115,
             // "labelRotation": 45,
             "axisAlpha": 0,
              "fillColor": "#000000",
@@ -564,7 +566,7 @@ function lineChartWithBaseLine(chartData,legends,baselineScore,container,xaxisLa
           "categoryAxis": {
               "gridPosition": "start",
               "markPeriodChange": false,
-              "minHorizontalGap": 100,
+              "minHorizontalGap": 115,
               // "labelRotation": 45,
               "axisAlpha": 0,
                "fillColor": "#000000",
@@ -655,7 +657,7 @@ function shadedLineChartWithBaseLine(chartData,label,baseLine,container,xaxisLab
          "categoryAxis": {
              "gridPosition": "start",
              "markPeriodChange": false,
-              "minHorizontalGap": 100,
+              "minHorizontalGap": 115,
              "axisAlpha": 0,
               "fillColor": "#000000",
              "gridAlpha": 0,
