11a469be6fab1fb15c09e862a119883916846dba
Prajay Verenkar <prajay@ajency.in>
2016-04-11 16:29:47 +0530

    bug

--- app/Http/Controllers/Project/SubmissionController.php
+++ app/Http/Controllers/Project/SubmissionController.php
@@ -137,7 +137,7 @@ class SubmissionController extends Controller
               $completedResponses[] = $response;
             }
 
-            if($reviewStatus=='reviewed_no_action' || $reviewStatus=='reviewed_call_done' || $reviewStatus=='reviewed_appointment_fixed') {
+            if($reviewed=='reviewed_no_action' || $reviewed=='reviewed_call_done' || $reviewed=='reviewed_appointment_fixed') {
                 // echo $sequenceNumber.'<br>';
                 $datediff =0;
                 $datediff = abs( strtotime( $updatedAt ) - strtotime( $createdAt ) ) / 3600;
