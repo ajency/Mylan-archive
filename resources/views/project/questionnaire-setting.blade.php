d510a0cb04b43cd92db76217a5223cc7300dcde9
nevesseq <neves@ajency.in>
2016-06-17 17:39:04 +0530

    notify alert added for questionary setting form

--- resources/views/project/questionnaire-setting.blade.php
+++ resources/views/project/questionnaire-setting.blade.php
@@ -58,6 +58,15 @@
             </div>
             <hr class="m-t-0">
             
+            <div class="row">
+              <div class="col-md-9">
+                <div class="alert alert-warning">
+                    <i class="fa fa-exclamation-triangle"></i> Grace Period should be <strong>less than half of the frequency</strong> and reminder time should be <strong>less than grace period</strong>
+                </div>
+              </div>
+            </div>
+            
+            
 
                   
                   
