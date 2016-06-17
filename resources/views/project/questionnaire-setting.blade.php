db5b4e8c1224d6831d34670bd17cc9d8e9db5b9e
nevesseq <neves@ajency.in>
2016-06-17 17:00:52 +0530

    changes in create questionary form

--- resources/views/project/questionnaire-setting.blade.php
+++ resources/views/project/questionnaire-setting.blade.php
@@ -30,29 +30,29 @@
                       <hr>
           @include('admin.flashmessage')
 
-            <form class="form-horizontal col-sm-12 mri-form setQuestion" method="post" action="{{ url( $hospital['url_slug'].'/'.$project['project_slug'].'/'.$action.'/' ) }}" data-parsley-validate onsubmit="return validatefrequencySettings(true);">
+            <form class="col-sm-12 mri-form setQuestion" method="post" action="{{ url( $hospital['url_slug'].'/'.$project['project_slug'].'/'.$action.'/' ) }}" data-parsley-validate onsubmit="return validatefrequencySettings(true);">
 
             
             <div class="row">
               <div class="col-sm-6">
                 <div class="form-group">
-                  <label for="editable" class="col-sm-4 side-label">Name</label>
-                  <div class="col-sm-6" style="padding-right: 0;">
+                  <label for="editable" class="side-label bold">Name</label>
+                  
                   <input type="text" name="name" id="name" class="nameField"  value="{{ $settings['name'] }}" data-parsley-required>
-                  </div>
+                  
                 </div>
               </div>
               <div class="col-sm-6">
                 <div class="form-group">
-                  <label for="editable" class="col-sm-4 side-label text-right">Editable</label>
-                  <div class="col-sm-6">
-                  <div class="radio">
+                  <label for="editable" class="side-label bold">Editable</label>
+                  
+                  <div class="radio p-t-10">
                   <input id="YES" type="radio" name="editable" value="yes" checked="checked">
                   <label for="YES">Yes</label>
                   <input id="NO" type="radio" name="editable" value="no" {{ ($settings['editable']==false)?'checked':'' }}>
                   <label for="NO">No</label>
                   </div>
-                  </div>
+                  
                 </div>
               </div>
             </div>
@@ -84,10 +84,11 @@
                   @endif
 
                   <div class="questionnaire-setting">
-                    <div class="form-group m-b-0">
+                    <div class="form-group clearfix">
  
-                      <label for="frequency" class="col-sm-2 side-label">Frequency</label>
-                      <div class="col-sm-5">
+                      <label for="frequency" class="side-label bold m-b-0">Frequency</label>
+                      <span class="help-block m-t-5">Specify the interval after which the patient should be able to answer the questionnaire again.</span>
+                      <div class="col-sm-5 p-l-0">
                         <input type="text" name="frequencyDay" class="form-control input-days" id="frequency" placeholder="No. of" value="{{ $settings['frequency']['day'] }}"  data-parsley-validation-threshold="1" data-parsley-trigger="keyup" 
                       data-parsley-type="digits"> <h6 class="seconds">days</h6>
                       </div>
@@ -97,12 +98,13 @@
                       data-parsley-type="digits"><h6 class="seconds">hours</h6>
                       </div>
                     </div>
-                    <span class="col-md-offset-2 help-block m-t-5 m-b-25">Specify the interval after which the patient should be able to answer the questionnaire again.</span>
+                    
 
 
-                   <div class="form-group m-b-0">
-                      <label for="gracePeriod" class="col-sm-2 side-label">Grace Period</label>
-                      <div class="col-sm-5">
+                   <div class="form-group clearfix">
+                      <label for="gracePeriod" class="side-label bold m-b-0">Grace Period</label>
+                      <span class="help-block m-t-5">The time duration in which a patient is allowed to answer a questionnaire after it is Due.</span>
+                      <div class="col-sm-5 p-l-0">
                         <input type="text" class="form-control input-days" id="gracePeriod" name="gracePeriodDay" placeholder="No .of" value="{{ $settings['gracePeriod']['day'] }}"  data-parsley-validation-threshold="1" data-parsley-trigger="keyup" 
                       data-parsley-type="digits">
                         <h6 class="seconds">days</h6>
@@ -112,11 +114,12 @@
                       data-parsley-type="digits"><h6 class="seconds">hours</h6>
                       </div>
                     </div>
-                     <span class="col-md-offset-2 help-block m-t-5 m-b-25">The time duration in which a patient is allowed to answer a questionnaire after it is Due.</span>
+                     
 
-                   <div class="form-group m-b-0">
-                    <label for="reminderTime" class="col-sm-2 side-label">Reminder Time</label>
-                    <div class="col-sm-5">
+                   <div class="form-group clearfix">
+                    <label for="reminderTime" class="side-label bold m-b-0">Reminder Time</label>
+                    <span class="help-block m-t-5">Mention the time period before the next occurrence when the reminder notification should be triggered.</span>
+                    <div class="col-sm-5 p-l-0">
                       <input type="text" class="form-control input-days" name="reminderTimeDay" id="reminderTime" placeholder="No. of" value="{{ $settings['reminderTime']['day'] }}"  data-parsley-validation-threshold="1" data-parsley-trigger="keyup" 
                     data-parsley-type="digits">
                       <h6 class="seconds">days</h6>
@@ -129,13 +132,12 @@
 
                   </div>
 
-                  <span class="col-md-offset-2 help-block m-t-5 m-b-25">Mention the time period before the next occurrence when the reminder notification should be triggered.</span>
+                  
 
 
                   <div class="form-group questActions">
                     <div class="col-sm-12 text-center mri-submit p-t-25">
                     <input type="hidden" value="{{ csrf_token()}}" name="_token"/>
-                    <input type="hidden" value="next_page" name="{{ $nextPage }}"/>
                       <button type="submit" class="btn btn-primary"><i class="fa fa-check" aria-hidden="true"></i> Save </button>
 
                       @if($settings['status'] =="published")
