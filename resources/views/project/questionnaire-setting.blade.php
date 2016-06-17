7ae0aca5a6ce6a4b47e6d90fc697cc6cb693038d
nevesseq <neves@ajency.in>
2016-06-17 17:05:48 +0530

    minor change

--- resources/views/project/questionnaire-setting.blade.php
+++ resources/views/project/questionnaire-setting.blade.php
@@ -34,7 +34,7 @@
 
             
             <div class="row">
-              <div class="col-sm-6">
+              <div class="col-sm-5">
                 <div class="form-group">
                   <label for="editable" class="side-label bold">Name</label>
                   
