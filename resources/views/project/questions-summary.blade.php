2da7d621526c6d541b52b16dc70fb09041af67ee
nevesseq <neves@ajency.in>
2016-06-22 13:05:40 +0530

    publish form markup pushed

--- resources/views/project/questions-summary.blade.php
+++ resources/views/project/questions-summary.blade.php
@@ -34,15 +34,24 @@
             $i=0;
             ?>
             @foreach($questionsList as $questionId => $question)
-                <div class="row questionSummary">
-                   <div class="col-md-12 questionSummary__head">
+                <div class="row questionSummary accord-questionSummary">
+
+
+
+                  <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">
+                   <div class="col-md-12 questionSummary__head clearfix">
                      <div>
-                        <span class="text-center semi-bold">{{ $question['title'] }} :- </span>
+                     <span class="chev-icons"></span>
+                        <span class="text-center semi-bold ttuc">{{ $question['title'] }} </span>
                             {{ $question['question'] }}
+                            <span class="label label-default pull-right m-t-5">4 OPTIONS</span>
                      </div>
-                   </div>                                   
-                   <div class="col-md-8 col-md-offset-2 questionSummary__options">
-                   @if(isset($optionsList[$questionId]))
+                   </div>
+                   </a>
+                  
+                  
+                   <div class="col-md-12 questionSummary__options panel-collapse collapse in m-b-15" id="collapseOne">
+                   <!-- @if(isset($optionsList[$questionId]))
                    <span class="text-center semi-bold col-md-2">Options</span>
                      <div class="col-md-10">
                         <ul>
@@ -51,9 +60,45 @@
                             @endforeach
                         </ul>
                      </div>
-                     @endif
-                   </div> 
-                </div>
+                     @endif -->
+                     <div class="row gray-header">
+                       <div class="col-sm-7">Options</div>
+                       <div class="col-md-2 text-center">Score</div>
+                       <div class="col-sm-2 text-center">Sub Question</div>
+                     </div>
+                    
+                    <div class="accord-subquetions-container">
+                    <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">
+                     <div class="row accord-subquestions">
+                       <div class="col-sm-7">
+                       <span class="chev-icons"></span>
+                         pain still there, even after medication 
+                       </div>
+                       <div class="col-sm-2 text-center">
+                         <span class="bold">4</span>
+                       </div>
+                       <div class="col-sm-2 text-center">
+                         <div class="checkbox p-t-0">
+                            <label>
+                              <input type="checkbox">
+                            </label>
+                          </div>
+                       </div>
+                     </div><!--/accord-subquestions-->
+                     </a>
+                     <div class="row panel-collapse collapse accord-questions" id="collapseTwo">
+                     <div class="col-sm-11 col-md-offset-1 gray-accord-questions">
+                        <span class="ttuc">PAIN</span>
+                        which statement describe best of your pain in last month?
+                        <span class="label label-default pull-right m-t-5">4 OPTIONS</span>
+                     </div>
+                     </div>
+                     </div><!--/accord-subquestions-container-->
+
+                   </div> <!-- /questionSummary__options -->
+                   
+                   
+                </div><!--/accord-questionSummary-->
                 
            <?php $i++; ?>
             @endforeach
