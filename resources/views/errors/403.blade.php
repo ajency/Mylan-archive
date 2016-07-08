6ceef36d7ba75761c8a72d31ae8e8ea7fb48a8a7
nevesseq <neves@ajency.in>
2016-07-08 11:58:23 +0530

    404, 403 page styled

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -3,44 +3,59 @@
     <head>
         <title>403.</title>
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'>
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
             body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+                font-family: 'Open Sans';
             }
 
             .content {
                 text-align: center;
-                display: inline-block;
+                position: relative;
             }
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
+            .error-403{
+                font-size: 150px;
+                font-weight: 600;
+                color: #ccc;
+                opacity: .3;
+                margin-bottom: 0;
+            }
+
+            .error-subtitle{
+                position: absolute;
+                top: 80px;
+                left: -30px;
+                right: 0;
+                font-size: 16px;
+                letter-spacing: .5px;
+                
+            }
+
+            .actual-msg{
+                font-size: 14px;
+                word-spacing: .5px;
+            }
+
+            .cust-link{
+                color: #05a8a5;
+            }
+
+            .content p{
+                color: #5c5c5c;
             }
         </style>
     </head>
     <body>
+        
         <div class="container">
             <div class="content">
-                <div class="title">Invalid Access.</div>
+                <h1 class="error-403">403.</h1>
+                <p class="error-subtitle">ACCESS DENIED!</p>
+
+                <p class="actual-msg">You don't have enough permissions to access this page</p>
+                <a href="" class="btn btn-link cust-link">Go back</a>
             </div>
         </div>
     </body>
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,44 +5,60 @@
         <title>404.</title>
  
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        
+        <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,800' rel='stylesheet' type='text/css'>
 
         <style>
-            html, body {
-                height: 100%;
-            }
-
             body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                color: #B0BEC5;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
+                font-family: 'Open Sans';
             }
 
             .content {
                 text-align: center;
-                display: inline-block;
+                position: relative;
             }
 
-            .title {
-                font-size: 72px;
-                margin-bottom: 40px;
+            .error-404{
+                font-size: 150px;
+                font-weight: 600;
+                color: #ccc;
+                opacity: .3;
+                margin-bottom: 0;
             }
+
+            .error-subtitle{
+                position: absolute;
+                top: 80px;
+                left: -30px;
+                right: 0;
+                font-size: 16px;
+                letter-spacing: .5px;
+                
+            }
+
+            .actual-msg{
+                font-size: 14px;
+                word-spacing: .5px;
+            }
+
+            .cust-link{
+                color: #05a8a5;
+            }
+
+            .content p{
+                color: #5c5c5c;
+            }
+
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">404 ERROR.</div>
+                <h1 class="error-404">404.</h1>
+                <p class="error-subtitle">OOPS! SOMETHING IS MISSING FOLKS!</p>
+
+                <p class="actual-msg">This page does not exist. Maybe you took a wrong turn?</p>
+                <a href="" class="btn btn-link cust-link">Go back</a>
             </div>
         </div>
     </body>
