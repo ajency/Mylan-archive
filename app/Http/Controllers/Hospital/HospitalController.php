5b87d5b852b54dea1f0dcd7fe86e5056cc2c268e
Prajay Verenkar <prajay@ajency.in>
2016-07-08 11:14:31 +0530

    user login links

--- app/Http/Controllers/Hospital/HospitalController.php
+++ app/Http/Controllers/Hospital/HospitalController.php
@@ -55,7 +55,7 @@ class HospitalController extends Controller
      */
     public function show($hospitalSlug)
     {
-        return redirect()->intended($hospitalSlug.'/projects');
+        return redirect($hospitalSlug.'/projects');
         
         $inputs = Input::get(); 
         // $projectId = (isset($inputs['projectId']))?$inputs['projectId']:0;
