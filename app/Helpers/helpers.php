609c0e1388aa352f5b1f12f78a1033f87a482356
trilokajency <trilok@ajency.in>
2016-07-29 15:31:35 +0530

    changes

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -529,6 +529,10 @@ function getBrowser()
     { 
         $bname = 'Netscape'; 
         $ub = "Netscape"; 
+    }else{
+        $u_agent = "Mozilla/5.0 (compatible; MSIE 11.0; Windows NT 6.2; Trident/6.0)";
+        $bname = 'Other'; 
+        $ub = "Other"; 
     }
     
     // finally get the correct version number
