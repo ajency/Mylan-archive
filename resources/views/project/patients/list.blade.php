bc5ea79345db2725b9c31104ee3b006ec5030b33
trilokajency <trilok@ajency.in>
2016-08-04 16:11:50 +0530

    ext

--- resources/views/project/patients/list.blade.php
+++ resources/views/project/patients/list.blade.php
@@ -137,8 +137,8 @@
                      <thead class="cf">
                        <tr>
 
-                          <th width="11%">Patient ID</th>
-                          <th width="22%" class="sorting">Total Submissions<br>
+                          <th width="10%">Patient ID</th>
+                          <th width="0%" class="sorting">Total Submissions<br>
                               <sm class="sortPatientSummary" sort="completed" sort-type="asc" >Completed <i class="fa fa-angle-down sortCol"></i></sm>
                               <sm class="sortPatientSummary" sort="late" sort-type="asc" >Late <i class="fa fa-angle-down sortCol"></i></sm>
                               <sm class="sortPatientSummary" sort="missed" sort-type="asc" >Missed <i class="fa fa-angle-down sortCol"></i></sm>
