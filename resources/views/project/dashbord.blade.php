e183f662541d77861f61a350ef1b8518dc631b8d
trilokajency <trilok@ajency.in>
2016-08-04 12:20:56 +0530

    added email from extension

--- resources/views/project/dashbord.blade.php
+++ resources/views/project/dashbord.blade.php
@@ -539,9 +539,9 @@
                            <th class="sorting">Graph <br> 
                                <sm>  <i class="fa fa-circle baseline-color"></i> Baseline   &nbsp; &nbsp;<i class="fa fa-circle theme-color"></i> Total Score</sm>
                                        </th>
-                       <!--     <th>
+                            <th>
                               Action
-                           </th> -->
+                           </th>
                         </tr>
                      </thead>
                      <tbody id="patientSummaryData" limit="5">
@@ -622,7 +622,13 @@
                                  <div id="chart_mini_{{ $patientId }}" style="vertical-align: middle; display: inline-block; width: 130px; height: 35px;"></div>
                               </div>
                            </td>
-              
+                           <td>
+                              @if($patientSummary['late'] != 0)
+                               <span class="patient-mail{{ $referenceCode }}">
+                                
+                              </span>  
+                              @endif  
+                            </td> 
                         </tr>
                          
                         @endforeach
