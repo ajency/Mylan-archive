6d1ca44240a627d1b4381f4fd3dfa35d74a07f8c
trilokajency <trilok@ajency.in>
2016-08-29 13:01:41 +0530

    changes pushed

--- resources/views/project/submissions-view.blade.php
+++ resources/views/project/submissions-view.blade.php
@@ -238,25 +238,27 @@ Previous <span class="p-l-r-5">|</span> Baseline
 
   <div class="bb"></div>
 
-  <div class="row text-center">
-    <div class="col-sm-6">
-      <p class="m-b-0 lh18">
-        <i class="fa fa-flag text-{{ $responseData['baseLineFlag'] }}"></i>
-      </p>
-      <p class="fw600 fosz14 lh18">
-        Current vs Baseline
-      </p>
+    <div class="row text-center">
+
+      <div class="col-sm-6">
+        <p class="m-b-0 lh18">
+          <i class="fa fa-flag text-{{ $responseData['previousFlag'] }} {{ ($responseData['previousFlag']=='')?'hidden':'' }}"></i>
+        </p>
+        <p class="fw600 fosz14 lh18">
+          Current vs Previous
+        </p>
+      </div> 
+
+      <div class="col-sm-6">
+        <p class="m-b-0 lh18">
+          <i class="fa fa-flag text-{{ $responseData['baseLineFlag'] }}"></i>
+        </p>
+        <p class="fw600 fosz14 lh18">
+          Current vs Baseline
+        </p>
+      </div>
+      
     </div>
-    
-    <div class="col-sm-6">
-      <p class="m-b-0 lh18">
-        <i class="fa fa-flag text-{{ $responseData['previousFlag'] }} {{ ($responseData['previousFlag']=='')?'hidden':'' }}"></i>
-      </p>
-      <p class="fw600 fosz14 lh18">
-        Current vs Previous
-      </p>
-    </div>
-  </div>
 </div>
 
 </div>
