c1531b834d1015ac58b336258d51ef7264a6f849
nevesseq <neves@ajency.in>
2016-08-30 15:52:59 +0530

    read more added to submission and dashboard notes content

--- resources/views/project/submissions-view.blade.php
+++ resources/views/project/submissions-view.blade.php
@@ -190,7 +190,7 @@ $currUrl = $_SERVER['REQUEST_URI'];
         @if($responseData['reviewNote']!='')
 
         <div class="Notes">
-          <label class="fln">Notes: {{ $responseData['reviewNote'] }}</label> 
+          <label class="fln more">Notes: {{ $responseData['reviewNote'] }}</label> 
         </div>
 
         @endif
@@ -273,6 +273,10 @@ Previous <span class="p-l-r-5">|</span> Baseline
 <br>
 
 <div class="user-description-box">
+  <div class="clearfix" style="padding: 10px 30px;">
+    <div class="pull-left fw600 fosz14">Response</div>
+    <div class="pull-rigth fw600 fosz14">Previous | Baseline</div>
+  </div>
   <?php 
   $i=1;
   $firstBreak = 0;
@@ -523,6 +527,45 @@ setInterval(function(){
 }, 3000);   
 });
 }); 
+
+// more-less
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 150;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "read more";
+    var lesstext = "read less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
 </script> 
 
 <style>
