bc6aa7821fd90173b37929ada99babbbb545f75b
nevesseq <neves@ajency.in>
2016-08-31 09:22:46 +0530

    read more added for submission page

--- resources/views/project/patients/submission-notifications.blade.php
+++ resources/views/project/patients/submission-notifications.blade.php
@@ -110,7 +110,7 @@ $currUrl = $_SERVER['REQUEST_URI'];
                     <sm><b>#{{ $submissionNotification['sequenceNumber'] }}</b></sm>
                   </td>
                   <td class="text-center text-success">{{ sprintf($submissionNotification['msg'], $submissionNotification['previousTotalRedFlags'],$submissionNotification['sequenceNumber'] ) }}</td> 
-                  <td class="text-center">{{ $submissionNotification['reviewNote'] }}</td>
+                  <td class="text-center"><div class="more">{{ $submissionNotification['reviewNote'] }}</div></td>
                   <td class="text-center text-success">
                     <!-- <div class="submissionStatus" @if(strlen($submissionNotification['reviewStatus']) >10 ) data-toggle="tooltip" @endif data-placement="top" title="{{ getStatusName($submissionNotification['reviewStatus']) }}">{{ getStatusName($submissionNotification['reviewStatus']) }}</div> -->
                     <div class="submissionStatus" style="width: 100%;">{{ getStatusName($submissionNotification['reviewStatus']) }}</div>
@@ -208,6 +208,44 @@ setInterval(function(){
 }, 3000);   
 });
 });  
+
+// more-less
+$(document).ready(function() {
+    // Configure/customize these variables.
+    var showChar = 100;  // How many characters are shown by default
+    var ellipsestext = "...";
+    var moretext = "read more";
+    var lesstext = "read less";
+    
+
+    $('.more').each(function() {
+        var content = $(this).html();
+ 
+        if(content.length > showChar) {
+ 
+            var c = content.substr(0, showChar);
+            var h = content.substr(showChar, content.length - showChar);
+ 
+            var html = c + '<span class="moreellipses">' + ellipsestext+ '&nbsp;</span><span class="morecontent"><span>' + h + '</span>&nbsp;&nbsp;<a href="" class="morelink text-info">' + moretext + '</a></span>';
+ 
+            $(this).html(html);
+        }
+ 
+    });
+ 
+    $(".morelink").click(function(){
+        if($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
 
 </script>
 
